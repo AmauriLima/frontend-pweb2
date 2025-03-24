acfe3345140aa49bd53ad9fb4bafa3e1fe1cf2ca
AmauriLima <amauri.plimaj@gmail.com>
2025-03-24 10:08:11 -0300

    config: adds react query config

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -1,8 +1,12 @@
+import { queryClient } from "@/application/shared/clients/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient} >
       <h1>Hello world</h1>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 };
